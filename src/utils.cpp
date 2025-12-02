f8e3df787cf23ad705ac31afe53e71693554439a
HenryLodge <hlcodes01@gmail.com>
2025-12-01 22:47:28 -0500

    add unimpl functions

---
+++ src/utils.cpp
@@ -0,0 +1,26 @@
+#include "utils.hpp"
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <termios.h>
+#include <unistd.h>
+
+namespace Utils {
+
+  std::vector<uint8_t> stringToBytes(const std::string& str) {
+    return;
+  }
+
+  std::string bytesToString(const std::vector<uint8_t>& bytes) {
+    return;
+  }
+
+  std::string bytesToHex(const std::vector<uint8_t>& bytes) {
+    return;
+  }
+
+  std::string readPassword(const std::string& prompt) {
+    return;
+  }
+
+}
