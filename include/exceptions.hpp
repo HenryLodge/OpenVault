cfb509d9872335a513a60b7109e1daa2c24ad37b
HenryLodge <hlcodes01@gmail.com>
2025-12-03 23:14:21 -0500

    add new exceptions

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -5,22 +5,47 @@
 #include <string>
 
 class CustomException : public std::exception {
-private:
+  private:
     std::string exception_message;
-    
-public:
-  explicit CustomException(const std::string& msg) : exception_message(msg) {}
-  virtual ~CustomException() noexcept {}
-  
-  virtual const char* what() const noexcept override {
-    return exception_message.c_str();
-  }
+
+  public:
+    explicit CustomException(const std::string &msg) : exception_message(msg) {}
+    virtual ~CustomException() noexcept {}
+
+    virtual const char *what() const noexcept override
+    {
+      return exception_message.c_str();
+    }
 };
 
 // exception for invalid entry
 class EntryException : public CustomException {
-public:
-  explicit EntryException(const std::string& msg) : CustomException("Entry Error: " + msg) {}
+  public:
+    explicit EntryException(const std::string &msg) : CustomException("Entry Error: " + msg) {}
+};
+
+// exception for cryptography function errors
+class CryptoException : public CustomException {
+  public:
+    explicit CryptoException(const std::string &msg) : CustomException("Cryptography Error: " + msg) {}
+};
+
+// exception for file errors
+class FileException : public CustomException {
+  public:
+    explicit FileException(const std::string &msg) : CustomException("File Error: " + msg) {}
+};
+
+// exception for invalid password
+class InvalidPasswordException : public CustomException {
+  public:
+    explicit InvalidPasswordException(const std::string &msg = "Invalid password") : CustomException(msg) {}
+};
+
+// exception for a corrupted vault
+class CorruptedVaultException : public CustomException {
+  public:
+    explicit CorruptedVaultException(const std::string &msg = "Vault data is corrupt") : CustomException(msg) {}
 };
 
 #endif
